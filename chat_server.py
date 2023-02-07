00d6849b3ef12e03c7e8d754ee6dc2753a2176b8
Christiaan Smith <ces050@bucknell.edu>
2023-02-06 21:28:43 -0500

    Chat server almost done

--- chat_server.py
+++ chat_server.py
@@ -4,18 +4,27 @@ import re
 import threading
 from client_list import ClientInfo
 
+'''returns the message to send to a user who used /list'''
+def list_clients(client, clients):
+    message = 'NAME             SERVER\n'
+    for users in clients:
+        message += clients[users].get_nickname() + '\n'
+    
+    return message
+
 '''
 udpSocket - UDP Socket
 message - Message to broadcast
-clients - list of clients to broadcast the message to
+clients - Dictionary of clients to broadcast the message to
 
-Broadcasts a message to a list of clients on a UDP Socket
+Broadcasts a message to a dict of clients on a UDP Socket.
+NEEDS TO BE A DICTIONARY
 '''
 def broadcast_message(udpSocket, message, clients):
     #consolidate to a broadcast message method
     print(message)
     for chat_client in clients:
-        udpSocket.sendto(bytes(message, 'utf-8'), chat_client)
+        udpSocket.sendto(bytes(message, 'utf-8'), clients[chat_client].get_address())
 
 
 '''
@@ -23,13 +32,36 @@ This method handles whenever a client sends something through a socket.
 
 Called as in thread from the server to handle individual users.
 '''
-def handle_client(data, caddress, udpSocket, clients):
+def handle_client(data, client, udpSocket, clients):
 
-    print('message: ' + str(caddress))
+    message = ''.join([char for char in str(data)[2:-3]])
 
-    broadcast_str = "[" + re.search(r"\, (.*?)\)", str(caddress)).group(1) + "] " + ''.join([char for char in str(data)[2:-3]]) + '\n'
+    nickname = client.get_nickname()
 
-    broadcast_message(udpSocket, broadcast_str, clients)
+    print('message: ' + str(message))
+    print('nickname: ' + nickname)
+
+    if(message == "/list"): #lists all online users
+        broadcast_str = list_clients(client, clients)
+        udpSocket.sendto(bytes(broadcast_str, 'utf-8'), client.get_address())
+
+    elif(message == "/quit"): #disconnects user
+        broadcast_str = "Disconnecting from server, Goodbye!"
+        udpSocket.sendto(bytes(broadcast_str, 'utf-8'), client.get_address())
+
+
+
+    elif(message[:6] == "/nick "):
+        new_name = message[6:]
+        
+        broadcast_str = client.get_nickname() + " has changed their nickname changed to " + new_name + "\n"
+        client.change_nickname(new_name)
+        broadcast_message(udpSocket, broadcast_str, clients)
+
+
+    else:
+        broadcast_str = "[" + nickname + "] " + message + '\n'
+        broadcast_message(udpSocket, broadcast_str, clients)
 
 
 def Server():
@@ -40,21 +72,28 @@ def Server():
 
     print('listening for messages yuh')
 
-    clients = [] #list of client caddresses
-    client_info = [] #list of ClientInfo objects to make command integration easier
+    welcome_file = open("welcome.txt", encoding="utf8")
+    welcome_msg = welcome_file.read()
+    welcome_file.close()
+
+    print('Welcome message loaded')
+
+    clients = {} #list of client caddresses
     while True:
         data, caddress = udpSocket.recvfrom(4096)
 
         if caddress not in clients:
             new_client = ClientInfo(caddress)
-            client_info = []
-            clients.append(caddress)
+            clients[caddress] = new_client
 
             #Send Welcome Message :)
-            udpSocket.sendto(bytes("Welcome to my UDP Chat Server!!!\n", 'utf-8'), caddress)
+            udpSocket.sendto(bytes(welcome_msg, 'utf-8'), clients[caddress].get_address())
+        
+            broadcast_message(udpSocket, (clients[caddress].get_nickname() + " has connected to the server\n"), clients)
 
+        client = clients[caddress]
         #Creation of client threads, thus allowing multiple connections to the server
-        client_thread = threading.Thread(target=handle_client, args=(data, caddress, udpSocket, clients))
+        client_thread = threading.Thread(target=handle_client, args=(data, client, udpSocket, clients))
         client_thread.start()
 
 Server()
--- client_list.py
+++ client_list.py
@@ -1,12 +1,17 @@
+import re
 
 class ClientInfo():
 
-    def __init__(self, client_address, client_nickname = ''):
+    def __init__(self, client_address):
         self.client_address = client_address
-        self.client_nickname = client_nickname
+        self.client_nickname = "user" + re.search(r"\, (.*?)\)", str(client_address)).group(1)
 
-    def nickname(self, new_name):
+    def change_nickname(self, new_name):
         self.client_nickname = new_name
+
 
     def get_nickname(self):
         return self.client_nickname
+
+    def get_address(self):
+        return self.client_address
